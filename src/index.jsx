7a19e384c3342a34ac439d07d42ddaa1896f1c14
Pedro Lima <limma.pedro@gmail.com>
2023-03-08 22:04:21 -0300

    Create a react strucuture

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,17 @@
-import React from 'react';
-import App from './App';
+// VERSÕES > 18
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const container = document.getElementById('root'); 
+const root = createRoot(container);
+
+root.render(<App/>);
+
+
+// VERSÕES < 18
+// import { render } from 'react-dom';
+// import { App } from './App';
+
+// render(<App/>, document.getElementById('root'));
+
+// REFERENCE: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
