0f33425d87ef97f53fa5f6cbf74b309060e2325f
Pedro Lima <limma.pedro@gmail.com>
2023-03-12 01:59:48 -0300

    Added two new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles.scss';
 
 export function App () {
     // throw new Error("error"); testando source map
-    return <h1>Hello React JS</h1>
+    // return <h1>Hello React JS</h1>
+    return <RepositoryList />
 }
