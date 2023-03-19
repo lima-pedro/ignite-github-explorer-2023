50f60d6f550957dbabe04cd7f1704b357cb927e2
Pedro Lima <limma.pedro@gmail.com>
2023-03-19 20:54:17 -0300

    Added react hooks and updated version

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,38 @@
 import '../styles/repository-list.scss';
 
+import { useState, useEffect } from 'react';
+
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+// const repository = {
+//     name: 'unform',
+//     description: 'Forms in React',
+//     link: 'https://github.com/unform/unform'
+// }
+
+const URL = 'https://api.github.com/users/lima-pedro/repos';
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  async function getRepositories () {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setRepositories(data);
+  }
+  
+  useEffect(() => {
+    getRepositories();
+  }, [])
+
   return (
     <section className="repository-list">
     <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repo => (
+          <RepositoryItem key={repo.id} repository={repo} />
+        ))}
       </ul>
     </section>
   );
